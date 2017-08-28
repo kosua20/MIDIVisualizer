56c8c2aa68e39a19180f9e6a06d07e066257f81b
sirodrig <simon.rodriguez@inria.fr>
2017-08-28 10:28:27 +0100

    Change resources organization for faster compilation.

---
+++ src/ressources/data.h
@@ -0,0 +1,13 @@
+#ifndef DATA_RESSOURCES_H
+#define DATA_RESSOURCES_H
+
+#include <string>
+#include <map>
+#include <vector>
+
+extern const std::map<std::string, std::string> shaders;
+extern  unsigned char font_image[131072];
+extern  unsigned char particles_image[32352];
+extern  unsigned char flash_image[262144];
+
+#endif
