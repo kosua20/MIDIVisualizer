a53ae0bd430d30c6b96abceedeafff0092e107c2
Simon Rodriguez <kosua20@gmail.com>
2020-07-17 17:11:15 +0200

    Configuration: helpers to parse files and arguments.

---
+++ src/helpers/Configuration.cpp
@@ -0,0 +1,109 @@
+#include "Configuration.h"
+
+#include <iostream>
+#include <stdio.h>
+
+std::string trim(const std::string & str, const std::string & del) {
+	const size_t firstNotDel = str.find_first_not_of(del);
+	if(firstNotDel == std::string::npos) {
+		return "";
+	}
+	const size_t lastNotDel = str.find_last_not_of(del);
+	return str.substr(firstNotDel, lastNotDel - firstNotDel + 1);
+}
+
+Arguments Configuration::parseArguments(std::istream & configFile){
+	// Build arguments list.
+	std::map<std::string, std::vector<std::string>> args;
+	std::string line;
+	// Again, but this time splitting keys from arguments.
+	while(std::getline(configFile, line)){
+		// Remove extra spaces and dashes.
+		const std::string lineTrim = trim(line, " -\t\r");
+		if(lineTrim.empty()) {
+			continue;
+		}
+		// Split at first space.
+		const std::string::size_type keySep = lineTrim.find_first_of(" \t");
+		if(keySep == std::string::npos){
+			std::cerr << "Ignoring key " << keySep << " without arguments." << std::endl;
+			continue;
+		}
+		std::vector<std::string> values;
+		const std::string key = trim(lineTrim.substr(0, keySep), "-: \t");
+		// Split the rest of the line based on spaces only.
+		std::string::size_type beginPos = keySep + 1;
+		std::string::size_type afterEndPos = lineTrim.find_first_of(" \t", beginPos);
+		while(afterEndPos != std::string::npos) {
+			const std::string value = lineTrim.substr(beginPos, afterEndPos - beginPos);
+			values.push_back(value);
+			beginPos	= afterEndPos + 1;
+			afterEndPos = lineTrim.find_first_of(" \t", beginPos);
+		}
+		// There is one remaining value, the last one.
+		const std::string value = lineTrim.substr(beginPos);
+		values.push_back(value);
+
+		if(!key.empty() && !values.empty()) {
+			args[key] = values;
+		}
+	}
+	return args;
+}
+
+Arguments Configuration::parseArguments(const std::vector<std::string> & argv, bool & showHelp){
+	std::map<std::string, std::vector<std::string>> args;
+
+	const size_t argc = argv.size();
+	for(size_t aid = 1; aid < argc;) {
+		// Clean the argument from any -
+		const std::string arg = trim(argv[aid], "-:\t");
+		if(arg.empty()) {
+			continue;
+		}
+		++aid;
+		
+		// Note help and skip.
+		if(arg == "help"){
+			showHelp = true;
+			continue;
+		}
+		std::vector<std::string> values;
+
+		// While we do not encounter a dash, the values are associated to the current argument.
+		while(aid < argc) {
+			// If the argument begins with a double dash, it's the next argument.
+			if(argv[aid].size() >= 2 && argv[aid].substr(0, 2) == "--") {
+				break;
+			}
+			values.emplace_back(argv[aid]);
+			++aid;
+		}
+		if(values.empty()) {
+			std::cerr << "No values for key " << arg << std::endl;
+			continue;
+		}
+		args[arg] = values;
+	}
+	return args;
+}
+
+bool Configuration::parseBool(const std::string & str){
+	return str == "1" || str == "true" || str == "yes" || str == "True" || str == "Yes";
+}
+
+int Configuration::parseInt(const std::string & str){
+	return std::stoi(str);
+}
+
+float Configuration::parseFloat(const std::string & str){
+	return std::stof(str);
+}
+
+glm::vec3 Configuration::parseVec3(const std::vector<std::string> & strs){
+	glm::vec3 vec(0.0f);
+	for(size_t i = 0; i < std::min(size_t(3), strs.size()); ++i){
+		vec[i] = parseFloat(strs[i]);
+	}
+	return vec;
+}
---
+++ src/helpers/Configuration.h
@@ -0,0 +1,36 @@
+#ifndef Configuration_h
+#define Configuration_h
+
+#include <string>
+#include <vector>
+#include <map>
+#include <glm/glm.hpp>
+
+typedef std::map<std::string, std::vector<std::string>> Arguments;
+
+// Helper to trim characters from both ends of a string.
+std::string trim(const std::string & str, const std::string & del);
+
+class Configuration {
+
+public:
+
+	static Arguments parseArguments(std::istream & configFile);
+
+	/** Will filter arguments without values. */
+	static Arguments parseArguments(const std::vector<std::string> & argv, bool & showHelp);
+
+	static bool parseBool(const std::string & str);
+
+	static int parseInt(const std::string & str);
+
+	static float parseFloat(const std::string & str);
+
+	static glm::vec3 parseVec3(const std::vector<std::string> & strs);
+
+private:
+
+
+};
+
+#endif
