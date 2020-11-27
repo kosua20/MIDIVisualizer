0adba6a686251ba077f3dd579334516daa250e32
Simon Rodriguez <kosua20@gmail.com>
2020-11-27 22:54:05 +0100

    Framebuffer: don't resize if same size.

--- src/rendering/Framebuffer.cpp
+++ src/rendering/Framebuffer.cpp
@@ -60,15 +60,17 @@ void Framebuffer::unbind(){
 }
 
 void Framebuffer::resize(int width, int height){
-	_width = width;
-	_height = height;
-	// Resize the renderbuffer.
-	glBindRenderbuffer(GL_RENDERBUFFER, _idRenderbuffer);
-	glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT32F, _width, _height);
-	glBindRenderbuffer(GL_RENDERBUFFER, 0);
-	// Resize the texture.
-	glBindTexture(GL_TEXTURE_2D, _idColor);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+	if(width != _width || height != _height){
+		_width = width;
+		_height = height;
+		// Resize the renderbuffer.
+		glBindRenderbuffer(GL_RENDERBUFFER, _idRenderbuffer);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT32F, _width, _height);
+		glBindRenderbuffer(GL_RENDERBUFFER, 0);
+		// Resize the texture.
+		glBindTexture(GL_TEXTURE_2D, _idColor);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, _width, _height, 0, GL_RGBA, GL_UNSIGNED_BYTE, 0);
+	}
 	// Clear everything for safety;
 	bind();
 	glClear(GL_COLOR_BUFFER_BIT);
