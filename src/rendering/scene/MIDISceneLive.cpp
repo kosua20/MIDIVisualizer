a5d263bc37a3df4fb4a2b91269c8331af4cb5bf3
Simon Rodriguez <kosua20@gmail.com>
2020-11-29 23:44:40 +0100

    MIDIScene: add live play scene.
    Initial support for listing, connection and note events.

---
+++ src/rendering/scene/MIDISceneLive.cpp
@@ -0,0 +1,210 @@
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <glm/gtc/matrix_transform.hpp>
+
+#include "../../helpers/ProgramUtilities.h"
+#include "../../helpers/ResourcesManager.h"
+
+#include "MIDISceneLive.h"
+
+
+#ifdef _WIN32
+#undef MIN
+#undef MAX
+#endif
+
+#define MAX_NOTES_IN_FLIGHT 1024
+
+MIDISceneLive::~MIDISceneLive(){
+	shared().close_port();
+}
+
+MIDISceneLive::MIDISceneLive(int port) : MIDIScene(){
+	// For now we use the same MIDI in instance for everything.
+	if(shared().is_port_open()){
+		shared().close_port();
+	}
+	shared().open_port(port, "MIDIVisualizer input");
+
+	_activeIds.fill(-1);
+	_notes.resize(MAX_NOTES_IN_FLIGHT);
+	_notesCount = 0;
+}
+
+void MIDISceneLive::updateSets(const SetOptions & options){
+
+	//_midiFile.updateSets(options);
+
+	// Load notes shared data.
+	//	std::vector<float> data;
+	//	std::vector<MIDINote> notesM;
+	//	//_midiFile.getNotes(notesM, NoteType::MAJOR, 0);
+	//	for(auto& note : notesM){
+	//		data.push_back(float(note.note));
+	//		data.push_back(float(note.start));
+	//		data.push_back(float(note.duration));
+	//		data.push_back(0.0f);
+	//		data.push_back(float(note.set % CHANNELS_COUNT));
+	//	}
+	//
+	//	std::vector<MIDINote> notesm;
+	//	//_midiFile.getNotes(notesm, NoteType::MINOR, 0);
+	//	for(auto& note : notesm){
+	//		data.push_back(float(note.note));
+	//		data.push_back(float(note.start));
+	//		data.push_back(float(note.duration));
+	//		data.push_back(1.0f);
+	//		data.push_back(float(note.set % CHANNELS_COUNT));
+	//	}
+	//	// Upload to the GPU.
+	//	upload(data);
+}
+
+void MIDISceneLive::updatesActiveNotes(double time, double speed){
+	int minUpdated = MAX_NOTES_IN_FLIGHT;
+	int maxUpdated = 0;
+
+	// Update the particle systems lifetimes.
+	for(auto & particle : _particles){
+		// Give a bit of a head start to the animation.
+		particle.elapsed = (float(time) - particle.start + 0.25f) / (float(speed) * particle.duration);
+		// Disable particles that shouldn't be visible at the current time.
+		if(float(time) >= particle.start + particle.duration || float(time) < particle.start){
+			particle.note = -1;
+			particle.set = -1;
+			particle.duration = particle.start = particle.elapsed = 0.0f;
+		}
+	}
+
+	// Update all active notes, extending their duration.
+	for(size_t nid = 0; nid < _actives.size(); ++nid){
+		if(_actives[nid] < 0){
+			continue;
+		}
+		const int noteId = _activeIds[nid];
+		GPUNote & note = _notes[noteId];
+		note.duration = time - note.start;
+		// Keep track of which region was modified.
+		minUpdated = std::min(minUpdated, noteId);
+		maxUpdated = std::max(maxUpdated, noteId);
+	}
+
+	while(true){
+		auto message = shared().get_message();
+		if(message.size() == 0){
+			// End of the queue.
+			break;
+		}
+
+		const auto type = message.get_message_type();
+
+		if(message.is_note_on_or_off()){
+			short note = short(message[1]);
+
+			// If the note is currently active, disable it.
+			if(_actives[note] >= 0){
+				_actives[note] = -1;
+				// Keep the note as-is, complete.
+				// Duration has already been updated above.
+			}
+
+			// Now if this is an on event, we should start a new note.
+			if(type == rtmidi::message_type::NOTE_ON && message[2] > 0){
+				const int clamped = message.get_channel() % CHANNELS_COUNT;
+				const size_t index = _notesCount % MAX_NOTES_IN_FLIGHT;
+
+				// Activate the key.
+				_actives[note] = clamped;
+				_activeIds[note] = index;
+
+				auto & newNote = _notes[index];
+				newNote.start = time;
+				newNote.duration = 0.0f;
+				newNote.set = clamped;
+
+				const bool isMin = noteIsMinor[note % 12];
+				const short shiftId = (note/12) * 7 + noteShift[note % 12];
+				newNote.isMinor = isMin ? 1.0f : 0.0f;
+				newNote.note = float(shiftId);
+
+				// Keep track of which region was modified.
+				minUpdated = std::min(minUpdated, int(index));
+				maxUpdated = std::max(maxUpdated, int(index));
+
+				// Find an available particles system and update it with the note parameters.
+				for(auto & particle : _particles){
+					if(particle.note < 0){
+						// Update with new note parameter.
+						particle.duration = 10.0f; // Fixed value.
+						particle.start = time;
+						particle.note = note;
+						particle.set = clamped;
+						particle.elapsed = 0.0f;
+						break;
+					}
+				}
+
+				++_notesCount;
+			}
+
+		} else {
+			// Handle pedal.
+		}
+	}
+
+	_dataBufferSubsize = std::min(int(_notes.size()), _notesCount);
+
+	// If we have indeed updated a note.
+	if(minUpdated <= maxUpdated){
+		upload(_notes, minUpdated, maxUpdated);
+	}
+
+	_previousTime = time;
+
+	// Update pedal state.
+	_pedals.damper = _pedals.sostenuto = _pedals.soft = _pedals.expression = 0.0f;
+	//_midiFile.getPedalsActive(_pedals.damper, _pedals.sostenuto, _pedals.soft, _pedals.expression, time, 0);
+}
+
+double MIDISceneLive::duration() const {
+	return _previousTime;
+}
+
+double MIDISceneLive::secondsPerMeasure() const {
+	return 1.0;
+}
+
+int MIDISceneLive::notesCount() const {
+	return _notesCount;
+}
+
+void MIDISceneLive::print() const {
+
+}
+
+rtmidi::midi_in * MIDISceneLive::_sharedMIDIIn = nullptr;
+std::vector<std::string> MIDISceneLive::_availablePorts;
+int MIDISceneLive::_refreshIndex = 0;
+
+rtmidi::midi_in & MIDISceneLive::shared(){
+	if(_sharedMIDIIn == nullptr){
+		_sharedMIDIIn = new rtmidi::midi_in(rtmidi::API::UNSPECIFIED, "MIDIVisualizer");
+	}
+	return *_sharedMIDIIn;
+}
+
+const std::vector<std::string> & MIDISceneLive::availablePorts(){
+	if(_refreshIndex == 0){
+		const int portCount = shared().get_port_count();
+		_availablePorts.resize(portCount);
+
+		for(int i = 0; i < portCount; ++i){
+			_availablePorts[i] = shared().get_port_name(i);
+		}
+	}
+	// Only update once every 15 frames.
+	_refreshIndex = (_refreshIndex + 1) % 15;
+	return _availablePorts;
+}
---
+++ src/rendering/scene/MIDISceneLive.h
@@ -0,0 +1,48 @@
+#ifndef MIDISceneLive_h
+#define MIDISceneLive_h
+#include <gl3w/gl3w.h>
+#include <glm/glm.hpp>
+#include "../midi/MIDIBase.h"
+#include "../State.h"
+#include "MIDIScene.h"
+
+#include <rtmidi17/rtmidi17.hpp>
+
+class MIDISceneLive : public MIDIScene {
+
+public:
+
+	MIDISceneLive(int port);
+
+	void updateSets(const SetOptions & options);
+
+	~MIDISceneLive();
+
+	void updatesActiveNotes(double time, double speed);
+
+	double duration() const;
+
+	double secondsPerMeasure() const;
+
+	int notesCount() const;
+
+	void print() const;
+
+	static const std::vector<std::string> & availablePorts();
+
+private:
+
+	std::vector<GPUNote> _notes;
+	std::array<int, 128> _activeIds;
+	double _previousTime = 0.0;
+	int _notesCount = 0;
+
+	static rtmidi::midi_in & shared();
+
+	static rtmidi::midi_in * _sharedMIDIIn;
+	static std::vector<std::string> _availablePorts;
+	static int _refreshIndex;
+
+};
+
+#endif
