6edd3d92c8273c29419180d132799ecc94941944
Simon Rodriguez <kosua20@gmail.com>
2023-08-13 17:04:40 +0200

    Resources: store font SDF in texture.

--- src/resources/data.h
+++ src/resources/data.h
@@ -10,7 +10,7 @@ extern const std::unordered_map<std::string, std::string> shaders;
 
 extern  unsigned char flash_image[262144];
 extern  glm::vec2 flash_size;
-extern  unsigned char font_image[131072];
+extern  unsigned char font_image[2048000];
 extern  glm::vec2 font_size;
 extern  unsigned char particles_image[32352];
 extern  glm::vec2 particles_size;
